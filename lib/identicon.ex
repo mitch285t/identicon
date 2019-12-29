251209abba0d0fdcb35bfa70d35c4cc797b2cbfa
Mitchell Williams <mscottwilliams1994@gmail.com>
2019-12-29 12:49:26 -0600

    refactoring

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,9 +6,8 @@ defmodule Identicon do
   end 
 
   def pick_color(image) do 
-   %Identicon.Image{hex: hex_list} = image
-   [r, g, b | _tail] = hex_list
-   [r , g , b] 
+   %Identicon.Image{hex: [r, g, b | _tail]} = image
+    [r , g , b] 
   end 
 
 
